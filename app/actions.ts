d60da76bdca92dcf8632a18dc787a63496f18c6c
dannyk-dev <kruger.dkk@gmail.com>
2025-07-11 07:15:26 -0300

    added card activations

--- app/actions.ts
+++ app/actions.ts
@@ -3,8 +3,9 @@
 import { createSupabaseClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { encodedRedirect } from "@/utils/redirect";
-import {  ICardRequest } from "@/utils/types";
+import { ICardRequest } from "@/utils/types";
 import GenericError from "@/utils/error";
+import { revalidatePath } from "next/cache";
 
 export const signInAction = async (formData: FormData) => {
   const email = formData.get("email") as string;
@@ -48,43 +49,66 @@ export const signOutAction = async () => {
 
 export const saveCard = async (payload: ICardRequest): Promise<boolean> => {
   const client = await createSupabaseClient();
-  const { data: userData } = await client.auth.getUser()
+  const { data: userData } = await client.auth.getUser();
   const user_id = userData.user!.id;
 
-
-  const { error } = await client.from('cards').insert({
+  const { error } = await client.from("cards").insert({
     ...payload,
-    country_code: 'US',
-    user_id
+    country_code: "US",
+    user_id,
   });
 
   if (error) {
     console.log(error);
-    throw new GenericError('Failed to create card', 500);
+    throw new GenericError("Failed to create card", 500);
   }
 
   return true;
-}
+};
 
 export const getProfile = async (userId: string) => {
   const client = await createSupabaseClient();
-  const {data, error} = await client.from('profiles').select('*').eq('id', userId).single();
+  const { data, error } = await client
+    .from("profiles")
+    .select("*")
+    .eq("id", userId)
+    .single();
 
   if (!error) {
     return data;
   }
 
   throw error;
-}
+};
 
 export const getCards = async () => {
   const client = await createSupabaseClient();
   const { data: userData } = await client.auth.getUser();
   const profile = await getProfile(userData.user!.id);
-  console.log(profile.admin);
-  if (profile.admin){
-    return await client.from('cards').select('*')
+
+  if (profile.admin) {
+    return await client.from("cards").select("*").eq("active", true);
   }
 
-  return await client.from('cards').select('*').eq('user_id', userData.user!.id);
-}
+  return await client
+    .from("cards")
+    .select("*")
+    .eq("user_id", userData.user!.id)
+    .eq("active", true);
+};
+
+export const disableCard = async (id: number, active: boolean = false) => {
+  const client = await createSupabaseClient();
+
+  const result = await client
+    .from("cards")
+    .update({ active })
+    .eq("id", id)
+    .select();
+
+  if (!result.error) {
+    revalidatePath("/admin/cards");
+  }
+
+  return result.error;
+};
--- app/admin/cards/page.tsx
+++ app/admin/cards/page.tsx
@@ -15,23 +15,29 @@ async function CardsPage() {
   }
 
   return (
-    <Container title="My Cards" description="Manage your cards" RightComponents={(
-      <Button asChild variant='default'>
-        <Link href='/admin/cards/new'>
-          <PlusCircle />
-          Add Card
-        </Link>
-      </Button>
-    )}>
-      <div className="flex flex-col w-full mt-10 space-y-4">
-        <p className="mt-auto text-muted-foreground">
-          Found {data.length} results
-        </p>
-        <div className="flex flex-col w-full gap-y-4 ">
-{data.length > 0 &&
-          data.map((item) => <CardItem key={item.id} card={item as ICard} />)}
+    <Container
+      title="My Cards"
+      description="Manage your cards"
+      RightComponents={
+        <Button asChild variant="default">
+          <Link href="/admin/cards/new">
+            <PlusCircle />
+            Add Card
+          </Link>
+        </Button>
+      }
+    >
+      <div className="flex flex-col w-full mt-10 space-y-3">
+        <div className="flex items-center w-full justify-between">
+          <div className="">filters here</div>
+          <p className="mt-auto text-muted-foreground">
+            Found {data.length} results
+          </p>
         </div>
-
+        <div className="flex flex-col w-full gap-y-4 ">
+          {data.length > 0 &&
+            data.map((item) => <CardItem key={item.id} card={item as ICard} />)}
+        </div>
       </div>
     </Container>
   );
